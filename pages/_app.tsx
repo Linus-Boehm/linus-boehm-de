2940857f53b39b114f0fdc74f50920a62a1eab44
Linus Boehm <letters@linusboehm.de>
2021-11-28 22:24:32 +0100

    Remove google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,14 @@ import Head from 'next/head';
 import { GlobalStyles } from 'twin.macro';
 import { AppProps } from 'next/app';
 import React from 'react';
-import '@fontsource/yuji-boku';
-import '@fontsource/nunito';
+import "@fontsource/amatic-sc";
+import "@fontsource/josefin-sans";
 import { appWithTranslation } from 'next-i18next';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Josefin+Sans:wght@300&display=swap" rel="stylesheet" />
         <title>Linus Boehm</title>
       </Head>
       <GlobalStyles />
